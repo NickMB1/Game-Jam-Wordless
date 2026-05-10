8ed09931f692dc799e3687e85d458f780a966987
NickMB1 <NBellver@edu.unisinos.br>
2026-05-10 20:34:11 -0300

    Commit coletavel teste
    Um item coletavel de teste

---
+++ gamejam_wordless/Scripts/coletavel_teste.gd
@@ -0,0 +1,25 @@
+extends Area2D
+var coletavel = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if coletavel == true:
+		if Input.is_action_just_pressed("Interact"):
+			queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:	
+	coletavel = true
+	print("O player colidiu")
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	coletavel = false
+	print("O player saiu")
+	
